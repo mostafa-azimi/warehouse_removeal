91e0ae127dbe2249108d4c9ca7ddbe641c4def36
mostafa-azimi <201478367+mostafa-azimi@users.noreply.github.com>
2025-11-01 10:05:40 -0400

    deploy: Build and deploy

--- app/api/shiphero/product-locations/route.ts
+++ app/api/shiphero/product-locations/route.ts
@@ -46,7 +46,7 @@ export async function POST(request: NextRequest) {
                     sku
                     name
                   }
-                  locations(first: 75) {
+                  locations(first: 50) {
                     edges {
                       node {
                         quantity
